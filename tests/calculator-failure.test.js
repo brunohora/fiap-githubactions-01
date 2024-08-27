52e3326857d0e0521ce1f0f54d84c389a2c5913c
brunodahora <brunoshora@gmail.com>
2024-08-27 20:53:02 -0300

    Adicionando estratégia matrix em versão do Node.js

---
+++ tests/calculator-failure.test.js
@@ -0,0 +1,9 @@
+const calculator = require('../calculator')
+
+test('string with two commas should result in NaN', () => {
+    expect(calculator.add('2,,')).toBe(NaN);
+});
+
+test('string with a alphabetic character should result in NaN', () => {
+    expect(calculator.add('2,N')).toBe(NaN);
+});
--- tests/calculator-success.test.js
+++ tests/calculator-success.test.js
@@ -1,4 +1,4 @@
-const calculator = require('./calculator')
+const calculator = require('../calculator')
 
 test('string with a single number should result in the number itself', () => {
     expect(calculator.add('1,1')).toBe(2);
@@ -14,12 +14,4 @@ test('string with four numbers separated by comma should result in the sum of th
 
 test('string with four numbers separated by comma should result in the sum of the numbers', () => {
     expect(calculator.add('2,0,4,5')).toBe(11);
-});
-
-test('string with two commas should result in NaN', () => {
-    expect(calculator.add('2,,')).toBe(NaN);
-});
-
-test('string with a alphabetic character should result in NaN', () => {
-    expect(calculator.add('2,N')).toBe(NaN);
 });
